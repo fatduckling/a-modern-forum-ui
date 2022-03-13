2c5be1482fe6c72267b672c672b50bf11511aff8
Saleem Gagguturu <saleemgagguturu@live.com>
2022-03-13 23:24:47 +1100

    Added results graph

--- lib/widgets/buttons/rounded/small/small_rounded_button.dart
+++ lib/widgets/buttons/rounded/small/small_rounded_button.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 class SmallRoundedButton extends StatelessWidget {
   final String _text;
 
-  const SmallRoundedButton(this._text, {Key? key}) : super(key: key);
+  final VoidCallback? _onPressed;
+
+  const SmallRoundedButton(this._text, {Key? key, VoidCallback? onPressed})
+      : _onPressed = onPressed,
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +22,7 @@ class SmallRoundedButton extends StatelessWidget {
             ),
           ),
           child: Text(_text),
-          onPressed: () {},
+          onPressed: _onPressed ?? () {},
         ));
   }
 }
--- lib/widgets/threads/types/poll_thread.dart
+++ lib/widgets/threads/types/poll_thread.dart
@@ -1,8 +1,9 @@
 import 'package:a_modern_forum_project/widgets/buttons/rounded/small/small_rounded_button.dart';
 import 'package:a_modern_forum_project/widgets/icon_with_text/icon_with_text.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
-/// Displays an
+/// Displays a polling thread where users can vote and a graph is displayed after
 class PollThread extends StatefulWidget {
   const PollThread({Key? key}) : super(key: key);
 
@@ -11,7 +12,10 @@ class PollThread extends StatefulWidget {
 }
 
 class _PollThread extends State<PollThread> {
-  final List<bool> checkboxValues = [for (var i = 1; i <= 6; i++) i % 2 == 0];
+  /// If is true, the results will show, otherwise the voting option widget will show
+  bool _isShowingGraph = false;
+
+  final List<bool> _checkboxValues = [for (int i = 1; i <= 6; i++) i % 2 == 0];
 
   @override
   Widget build(BuildContext context) {
@@ -32,32 +36,87 @@ class _PollThread extends State<PollThread> {
               ],
             ),
             const Divider(),
-            ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: checkboxValues.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return CheckboxListTile(
-                    contentPadding: EdgeInsets.zero,
-                    controlAffinity: ListTileControlAffinity.leading,
-                    title: Text("Option $index"),
-                    //    <-- label
-                    value: checkboxValues[index],
-                    onChanged: (newValue) {
-                      bool disabled = newValue == null;
-                      bool isChecked = !disabled && newValue;
-                      setState(() {
-                        checkboxValues[index] = isChecked;
-                      });
-                    },
-                  );
-                }),
-            const SizedBox(
-              height: 10,
-            ),
-            const SmallRoundedButton("Vote"),
+            _isShowingGraph ? showResults() : showPollOptions()
           ],
         ));
   }
+
+  /// Widget that displays the poll options
+  Widget showPollOptions() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: _checkboxValues.length,
+            itemBuilder: (BuildContext context, int index) {
+              return CheckboxListTile(
+                contentPadding: EdgeInsets.zero,
+                controlAffinity: ListTileControlAffinity.leading,
+                title: Text("Option $index"),
+                value: _checkboxValues[index],
+                onChanged: (newValue) {
+                  bool disabled = newValue == null;
+                  bool isChecked = !disabled && newValue;
+                  setState(() {
+                    _checkboxValues[index] = isChecked;
+                  });
+                },
+              );
+            }),
+        const SizedBox(
+          height: 10,
+        ),
+        SmallRoundedButton(
+          "Vote",
+          onPressed: () {
+            setState(() {
+              _isShowingGraph = true;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  /// Widget that displays the poll results
+  Widget showResults() {
+    final List<charts.Series<dynamic, String>> seriesList = [
+      charts.Series<VotingData, String>(
+          id: "Sales",
+          domainFn: (VotingData votingData, _) => votingData.option,
+          measureFn: (VotingData votingData, _) => votingData.count,
+          labelAccessorFn: (VotingData data, _) => data.count.toString(),
+          data: [
+            VotingData("Yes I think it works", 121),
+            VotingData("No it won't work", 234),
+            VotingData("c", 21),
+            VotingData("d", 13),
+            VotingData("e", 44),
+            VotingData("f", 99),
+          ])
+    ];
+    return SizedBox(
+      height: 500,
+      child: charts.BarChart(
+        seriesList,
+        animate: true,
+        vertical: false,
+        barRendererDecorator: charts.BarLabelDecorator<String>(),
+        primaryMeasureAxis:
+            const charts.NumericAxisSpec(renderSpec: charts.NoneRenderSpec()),
+      ),
+    );
+  }
+}
+
+/// Simple data type to hold the poll options and its statistics
+class VotingData {
+  final String option;
+  final int count;
+
+  VotingData(this.option, this.count);
 }
