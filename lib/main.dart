f65ea0740fd392738b243295dfbc86d25cd538bb
Saleem Gagguturu <saleemgagguturu@live.com>
2022-03-28 16:15:09 +1100

    Adding comment box

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:a_modern_forum_project/observers/screen_resize_observer.dart';
 import 'package:a_modern_forum_project/observers/scroll_observer.dart';
-import 'package:a_modern_forum_project/routes/view_post.dart';
+import 'package:a_modern_forum_project/routes/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_portal/flutter_portal.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -41,9 +41,6 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ViewPostRoute(Container(
-      height: 200,
-      color: Colors.blue,
-    ));
+    return const HomeRoute();
   }
 }
